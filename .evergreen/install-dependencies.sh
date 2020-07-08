e0f2dab94bf53f8fa11daf0c9ccca2f11735aac7
Andreas Braun <git@alcaeus.org>
2020-07-08 10:21:48 +0200

    First shot

---
+++ .evergreen/install-dependencies.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+DIR=$(dirname $0)
+# Functions to fetch MongoDB binaries
+. $DIR/download-mongodb.sh
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+get_distro
+
+# See .evergreen/download-mongodb.sh for most possible values
+case "$DISTRO" in
+   cygwin*)
+      echo "Install Windows dependencies"
+      ;;
+
+   darwin*)
+      echo "Install macOS dependencies"
+      ;;
+
+   linux-rhel*)
+      echo "Install RHEL dependencies"
+      ;;
+
+   linux-ubuntu*)
+      echo "Install Ubuntu dependencies"
+      sudo apt-get install -y awscli || true
+      ;;
+
+   sunos*)
+      echo "Install Solaris dependencies"
+      sudo /opt/csw/bin/pkgutil -y -i sasl_dev || true
+      ;;
+
+   *)
+      echo "All other platforms..."
+      ;;
+esac
+
+# TODO: allow configuring which version is installed
+pecl install mongodb
+
+# Install composer
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+composer install
---
+++ .evergreen/run-tests.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+# Supported/used environment variables:
+#       AUTH                    Set to enable authentication. Defaults to "noauth"
+#       SSL                     Set to enable SSL. Defaults to "nossl"
+#       MONGODB_URI             Set the suggested connection MONGODB_URI (including credentials and topology info)
+#       MARCH                   Machine Architecture. Defaults to lowercase uname -m
+
+
+AUTH=${AUTH:-noauth}
+SSL=${SSL:-nossl}
+MONGODB_URI=${MONGODB_URI:-}
+
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+[ -z "$MARCH" ] && MARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
+
+echo "Running $AUTH tests over $SSL, connecting to $MONGODB_URI"
+
+# Run the tests, and store the results in a Evergreen compatible JSON results file
+case "$OS" in
+   *)
+      vendor/bin/phpunit -v
+      ;;
+esac
+
